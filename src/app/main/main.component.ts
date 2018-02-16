f879118c429690508faa0f31fbd2028757c31d1d
kLawrence <ultimate1352@gmail.com>
2018-02-16 11:01:09 +0900

    add naver maps

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollToService, ScrollToConfigOptions } from '@nicky-lenaers/ngx-scroll-to';
 
+declare var naver: any;
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -11,6 +13,18 @@ export class MainComponent implements OnInit {
   constructor(private _scrollToService: ScrollToService) { }
 
   ngOnInit() {
+    console.log(naver);
+    const mapOptions = {
+      center: new naver.maps.LatLng(37.559348, 126.927818),
+      zoom: 11
+    };
+
+    const map = new naver.maps.Map('map', mapOptions);
+
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(37.559348, 126.927818),
+      map: map
+    });
   }
 
   gotoLink(target) {
